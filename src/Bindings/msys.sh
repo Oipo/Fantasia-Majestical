1b34c5ef6b0672dcc68ea5229fc709ba48abcb79
Michael de Lang <kingoipo@gmail.com>
2010-12-26 03:07:23 +0100

    Converted to DOS line endigns

--- src/Bindings/msys.sh
+++ src/Bindings/msys.sh
@@ -7,7 +7,7 @@ QT="/c/Qt/2009.03"
 CFLAGS="-shared -O2 -Wall"
 DEFINES="-DQT_NO_DEBUG -DNDEBUG -D_REENTRANT -DQT_CORE_LIB -DQT_GUI_LIB -DQT_SHARED"
 INCLUDES="-I. -I${PYTHON} -I${QT}/qt/include -I${QT}/qt/include/qt -I${QT}/qt/include/QtCore -I${PYTHON}/include -I${PYTHON}/Lib/site-packages/PyQt4/include"
-LIBDIRS="-L${PYTHON}/libs -L${QT}/qt/lib "
+LIBDIRS="-L${PYTHON}/libs -L${QT}/qt/lib"
 
 echo "removing previously created files"
 rm -f moc_bMain.cpp sip* *.o
