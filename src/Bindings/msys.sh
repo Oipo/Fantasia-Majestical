f0e23895421cca6e2b2772470912bcce884ad9dd
Michael de Lang <kingoipo@gmail.com>
2010-12-26 02:54:58 +0100

    Fancified msys.h

--- src/Bindings/msys.sh
+++ src/Bindings/msys.sh
@@ -1,7 +1,26 @@
 
 # Contents will change a lot if versions are changed
 
-/c/Qt/2009.03/qt/bin/moc bMain.h -o moc_bMain.cpp
-/c/Python26/python.exe configure.py
-echo 'compiling...'
-g++ -shared -O2 -Wall -DQT_NO_DEBUG -DNDEBUG -W -D_REENTRANT -DQT_CORE_LIB -DQT_GUI_LIB -DQT_SHARED -I. -L/c/Python26/libs -L/c/Qt/2009.03/qt/lib -I/c/Python26 -I/c/Qt/2009.03/qt/include -I/c/Qt/2009.03/qt/include/qt -I/c/Qt/2009.03/qt/include/QtCore -I/c/Python26/include -I/c/Python26/Lib/site-packages/PyQt4/include *.cpp ../../qt4/release/librandom-game-generator.a -lpython26 -lQtCore4 -lQtGui4 -o _bmainmod.pyd
+PYTHON="/c/Python26"
+QT="/c/Qt/2009.03"
+
+CFLAGS="-shared -O2 -Wall"
+DEFINES="-DQT_NO_DEBUG -DNDEBUG -D_REENTRANT -DQT_CORE_LIB -DQT_GUI_LIB -DQT_SHARED"
+INCLUDES="-I. -I${PYTHON} -I${QT}/qt/include -I${QT}/qt/include/qt -I${QT}/qt/include/QtCore -I${PYTHON}/include -I${PYTHON}/Lib/site-packages/PyQt4/include"
+LIBDIRS="-L${PYTHON}/libs -L${QT}/qt/lib "
+
+echo "removing previously created files"
+rm -f moc_bMain.cpp sip* *.o
+
+echo "creating new files"
+$QT/qt/bin/moc bMain.h -o moc_bMain.cpp
+$PYTHON/python.exe configure.py
+
+for file in $(ls -1 *.o); do
+  output=`echo $file | sed 's/.cpp/.o/'`
+  echo "compiling $file into $output"
+  g++ -c $CFLAGS $DEFINES $INCLUDES` $file -o $output
+done
+
+echo "Generting pyd"
+g++ $CFLAGS $LIBDIRS *.o ../../qt4/release/librandom-game-generator.a -lpython26 -lQtCore4 -lQtGui4 -o _bmainmod.pyd
